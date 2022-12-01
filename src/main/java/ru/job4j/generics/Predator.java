9d10e5d17eac5737e0d20170a5f2a6f2aa19ae80
Yamakks <maxim5_91@mail.ru>
2022-12-01 14:02:40 +0300

    0. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generics/Predator.java
@@ -0,0 +1,7 @@
+package ru.job4j.generics;
+
+public class Predator extends Animal {
+    public Predator() {
+
+    }
+}
---
+++ src/main/java/ru/job4j/generics/Tiger.java
@@ -0,0 +1,7 @@
+package ru.job4j.generics;
+
+public class Tiger extends Predator {
+    public Tiger() {
+
+    }
+}
