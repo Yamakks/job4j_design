475a620880299de6f58f69fcee27fc6fd1d99d40
Yamakks <maxim5_91@mail.ru>
2022-12-08 15:20:51 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,50 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements LinkedList<E> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<E> head;
+
+    @Override
+    public void add(E value) {
+        Node<E> newNode = new Node<>(value, null);
+        if (head == null) {
+            head = newNode;
+
+        } else {
+            Node<E> bufNode = head;
+            while (bufNode.next != null) {
+                bufNode = bufNode.next;
+            }
+            bufNode.next = newNode;
+        }
+
+        size++;
+        modCount++;
+
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
