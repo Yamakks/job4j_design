7e23c53ee3008a1f940363360dc5446006c16be7
Yamakks <maxim5_91@mail.ru>
2022-12-16 13:37:43 +0300

    3. Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,85 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ForwardLinked<E> implements Iterable<E> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<E> head;
+
+    public void add(E value) {
+        Node<E> newNode = new Node<>(value, null);
+        size++;
+        modCount++;
+        if (head == null) {
+            head = newNode;
+
+        } else {
+            Node<E> bufNode = head;
+            while (bufNode.next != null) {
+                bufNode = bufNode.next;
+            }
+            bufNode.next = newNode;
+        }
+
+    }
+
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> bufNode = head;
+        for (int i = 1; i <= index; i++) {
+            bufNode = bufNode.next;
+        }
+        return bufNode.item;
+    }
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            Node<E> bufNode = head;
+            int mod = modCount;
+
+
+            @Override
+            public boolean hasNext() {
+                if (mod != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return bufNode != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Node<E> bufNodenext = bufNode;
+                bufNode = bufNode.next;
+                return bufNodenext.item;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+
+    public E deleteFirst() {
+        Node<E> deleted = head;
+        if (deleted == null) {
+            throw new NoSuchElementException();
+        }
+            head = deleted.next;
+            size--;
+            modCount++;
+        return deleted.item;
+    }
+}
