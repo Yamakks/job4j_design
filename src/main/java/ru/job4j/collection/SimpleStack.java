25f27e0da98be0060222682b120d310bd10f7f09
Yamakks <maxim5_91@mail.ru>
2022-12-22 15:21:49 +0300

    3. Удалить head в односвязном списке. [#51424]

--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -1,7 +1,7 @@
 package ru.job4j.collection;
 
 public class SimpleStack<T> {
-    private ForwardLinked<T> linked = new ForwardLinked<>();
+    private ForwardLinked<T> linked = new ForwardLinked<T>();
 
     public T pop() {
         return linked.deleteFirst();
