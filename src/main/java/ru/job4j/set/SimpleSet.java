6d3ac0e5d378b7500bba7629c5aa8d3b57633c90
maxim <maxim5_91@mail.ru>
2023-01-05 00:54:28 +0300

    1. Реализовать коллекцию Set на массиве [#996]

--- src/main/java/ru/job4j/set/SimpleSet.java
+++ src/main/java/ru/job4j/set/SimpleSet.java
@@ -9,12 +9,24 @@ public class SimpleSet<T> implements Set<T> {
 
     @Override
     public boolean add(T value) {
-        return false;
+        boolean rsl = false;
+            if (!contains(value)) {
+                set.add(value);
+                rsl = true;
+        }
+        return rsl;
     }
 
     @Override
     public boolean contains(T value) {
-        return false;
+        boolean rsl = false;
+        for (int i = 0; i < set.size(); i++) {
+            if (value == set.get(i)) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
     }
 
     @Override
---
+++ src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.set;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SimpleSetTest {
+    @Test
+    void whenAddNonNull() {
+        Set<Integer> set = new SimpleSet<>();
+        assertThat(set.add(1)).isTrue();
+        assertThat(set.contains(1)).isTrue();
+        assertThat(set.add(1)).isFalse();
+    }
+
+    @Test
+    void whenAddNull() {
+        Set<Integer> set = new SimpleSet<>();
+        assertThat(set.add(null)).isTrue();
+        assertThat(set.contains(null)).isTrue();
+        assertThat(set.add(null)).isFalse();
+    }
+
+}
