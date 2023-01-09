0f9dd7112dcdffc3ef77ca145746a50e2caa4033
maxim <maxim5_91@mail.ru>
2023-01-09 23:32:20 +0300

    7. Переопределить и equals и hashCode [#1002 #379111]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -21,7 +24,31 @@ public class User {
 
         User man1 = new User("user", 1, birthday);
         User man2 = new User("user", 1, birthday);
+        Map<User, Object> map = new HashMap<>();
+        map.put(man1, new Object());
+        map.put(man2, new Object());
+        for (Map.Entry<User, Object> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
 
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }
 
