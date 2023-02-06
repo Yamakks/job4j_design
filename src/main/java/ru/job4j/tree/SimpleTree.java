a2f136a0c88ea4c0ca84c426792dbe01d355987e
maxim <maxim5_91@mail.ru>
2023-02-06 23:07:07 +0300

    1. Создать элементарную структуру дерева [#1711]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -3,12 +3,28 @@ package ru.job4j.tree;
 import java.util.LinkedList;
 import java.util.Optional;
 import java.util.Queue;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
 
     public SimpleTree(final E root) {
         this.root = new Node<>(root);
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (condition.test(el)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
     }
 
     @Override
@@ -24,17 +40,11 @@ public class SimpleTree<E> implements Tree<E> {
 
     @Override
     public Optional<Node<E>> findBy(E value) {
-        Optional<Node<E>> rsl = Optional.empty();
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (el.value.equals(value)) {
-                rsl = Optional.of(el);
-                break;
-            }
-            data.addAll(el.children);
-        }
-        return rsl;
+        return findByPredicate(cond -> value.equals(cond.value));
+    }
+
+    @Override
+    public boolean isBinary() {
+        return findByPredicate(cond -> cond.children.size() > 2).isEmpty();
     }
 }
--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -3,6 +3,8 @@ package ru.job4j.tree;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SimpleTreeTest {
     @Test
@@ -32,6 +34,30 @@ class SimpleTreeTest {
         tree.add(4, 5);
         tree.add(5, 6);
         assertThat(tree.add(2, 6)).isFalse();
+    }
+
+    @Test
+    void whenBinaryIsTrue() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 5);
+        tree.add(2, 6);
+        tree.add(3, 7);
+        tree.add(3, 8);
+        assertTrue(tree.isBinary());
+    }
+
+    @Test
+    void whenBinaryIsFalse() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 5);
+        tree.add(2, 6);
+        tree.add(2, 7);
+        tree.add(3, 8);
+        assertFalse(tree.isBinary());
     }
 
 }
