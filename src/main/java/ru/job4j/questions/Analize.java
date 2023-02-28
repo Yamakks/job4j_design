1c5201d26d719efa6ccf6f083fe116a0d1c161ad
yamax <ZXSWO4cu4>
2023-02-28 14:15:29 +0300

    3. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/questions/Analize.java
@@ -0,0 +1,9 @@
+package ru.job4j.questions;
+
+import java.util.Set;
+
+public class Analize {
+    public static Info diff(Set<User> previous, Set<User> current) {
+
+    }
+}
---
+++ src/main/java/ru/job4j/questions/Info.java
@@ -0,0 +1,37 @@
+package ru.job4j.questions;
+
+public class Info {
+    private int added;
+    private int changed;
+    private int deleted;
+
+    public Info(int added, int changed, int deleted) {
+        this.added = added;
+        this.changed = changed;
+        this.deleted = deleted;
+    }
+
+    public int getAdded() {
+        return added;
+    }
+
+    public int getChanged() {
+        return changed;
+    }
+
+    public int getDeleted() {
+        return deleted;
+    }
+
+    public void setAdded(int added) {
+        this.added = added;
+    }
+
+    public void setChanged(int changed) {
+        this.changed = changed;
+    }
+
+    public void setDeleted(int deleted) {
+        this.deleted = deleted;
+    }
+}
