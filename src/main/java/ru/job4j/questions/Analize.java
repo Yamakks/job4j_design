828afc460d0acba3ca50296f5575835899f83d3b
yamax <ZXSWO4cu4>
2023-03-02 14:04:53 +0300

    3. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/questions/Analize.java
+++ src/main/java/ru/job4j/questions/Analize.java
@@ -1,5 +1,7 @@
 package ru.job4j.questions;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
@@ -8,23 +10,26 @@ public class Analize {
         int changes = 0;
         int deletes = 0;
         int adds = 0;
-        for (User prev : previous) {
-            if (!current.contains(prev)) {
-                deletes++;
-            }
-        }
+        Map<Integer, String> mapPrevious = new HashMap<>();
+        Map<Integer, String> mapCurrent = new HashMap<>();
         for (User currents : current) {
-            if (!previous.contains(currents)) {
+            mapCurrent.put(currents.getId(), currents.getName());
+        }
+        for (User prev : previous) {
+            mapPrevious.put(prev.getId(), prev.getName());
+        }
+        for (Map.Entry<Integer, String> curr : mapCurrent.entrySet()) {
+            if (!mapPrevious.containsKey(curr.getKey())) {
                 adds++;
             }
         }
-        for (User prev : previous) {
-            for (User currents : current) {
-                if (prev.getId() == currents.getId() && !Objects.equals(prev.getName(), currents.getName())) {
-                    changes++;
-                    adds--;
-                    deletes--;
-                }
+        for (Map.Entry<Integer, String> prev : mapPrevious.entrySet()) {
+            if (mapCurrent.containsKey(prev.getKey()) && !mapCurrent.containsValue(prev.getValue())) {
+                changes++;
+                continue;
+            }
+            if (!mapCurrent.containsKey(prev.getKey())) {
+                deletes++;
             }
         }
         return new Info(adds, changes, deletes);
