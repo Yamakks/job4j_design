08d245815f1ee9912028abac6002a3577cf53bce
yamax <ZXSWO4cu4>
2023-03-14 17:11:06 +0300

    2.0. FileOutputStream [#252488]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -7,9 +7,8 @@ public class ResultFile {
         try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
             for (int i = 0; i < 10; i++) {
                 for (int j = 0; j < 10; j++) {
-                    String s = String.valueOf((i + 1) * (j + 1));
-                    out.write(s.getBytes());
-                    out.write(System.lineSeparator().getBytes());
+                    String s = (String.valueOf((i + 1) * (j + 1)));
+                    out.write((s + " ").getBytes());
                 }
                 out.write(System.lineSeparator().getBytes());
             }
