99d96da6edb931a4e2aeee7b6f606f9b1022b5c4
yamax <ZXSWO4cu4>
2023-03-21 16:51:16 +0300

    7. Анализ доступности сервера [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -11,17 +11,14 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader in = new BufferedReader(new FileReader(source));
              BufferedWriter out = new BufferedWriter(new FileWriter(target))) {
-            boolean flag = false;
+            boolean flag = true;
             String line;
-            List<String> buf = new ArrayList<>();
             while ((line = in.readLine()) != null) {
-                if (flag != isOff(line)) {
-                    flag = isOff(line);
-                        buf.add(line.split(" ", 2)[1]);
+                if (isOff(line) == flag) {
+                    out.write(line, 4, line.length() - 4);
+                    out.append(";").append(flag ? "" : System.lineSeparator());
+                    flag = !flag;
                 }
-            }
-            for (int i = 0, j = 1; j < buf.size(); i = i + 2, j = j + 2) {
-                out.write(String.format("%s%s%s%n", buf.get(i), ";", buf.get(j)));
             }
             } catch (IOException e) {
             System.out.println("Ошибка при вводе/выводе данных из файла!");
