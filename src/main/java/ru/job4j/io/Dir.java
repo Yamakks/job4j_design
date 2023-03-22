5981186ea8a65abd43c90930d2d703a4f3b278f2
yamax <ZXSWO4cu4>
2023-03-22 13:41:27 +0300

    9.0. Файлы и директории. Класс File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,17 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.printf("size : %s%n", file.length());
+        System.out.printf("fileName : %s%n", file.getName());
+    }
+}
