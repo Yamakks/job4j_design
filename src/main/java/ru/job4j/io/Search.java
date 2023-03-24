b7dd950d5d976f85e659fac5085a65e5ebfdee16
maxim <maxim5_91@mail.ru>
2023-03-25 01:32:33 +0300

    11. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,14 +9,26 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js"))
-                .forEach(System.out::println);
+        if (validateArgs(args)) {
+            Path start = Paths.get(args[0]);
+            search(start, p -> p.toFile().getName().endsWith(args[1]))
+                    .forEach(System.out::println);
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static boolean validateArgs(String[] args) {
+        if (args[0].length() == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if (args[1].length() == 0) {
+            throw new IllegalArgumentException("Filename extension is null. Usage  FILENAME_EXTENSION.");
+        }
+        return true;
     }
 }
