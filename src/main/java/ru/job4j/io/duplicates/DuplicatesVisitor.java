34ecb2e644b2663fe5af0f6e893eab5a105062b6
yamax <ZXSWO4cu4>
2023-03-24 14:31:35 +0300

    11. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,12 +5,18 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
-        return super.visitFile(file, attrs);
-    }
+    private final HashMap<Integer, FileProperty> files = new HashMap<Integer, FileProperty>();
+    int i = 0;
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            i++;
+            files.put(i, new FileProperty((long) file.toFile().length(), file.getFileName().toString()));
+            return CONTINUE;
+        }
 }
