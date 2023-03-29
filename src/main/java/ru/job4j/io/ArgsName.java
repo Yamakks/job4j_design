5a6eb8bb9dadba938bcd3eca7b9552a7353fe671
yamax <ZXSWO4cu4>
2023-03-29 13:58:17 +0300

    13. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        /* TODO add the necessary checks. */
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        /* TODO parse args to values. */
+    }
+
+    public static ArgsName of(String[] args) {
+        /* TODO add the necessary checks. */
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
