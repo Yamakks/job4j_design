0e777ea17a873111e1face688d18ee4ea0d1c6dd
maxim <maxim5_91@mail.ru>
2023-04-03 22:40:28 +0300

    14. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,59 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    private static boolean validateArgs(String[] args) {
+
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Some args is null.");
+        }
+        Path path = Path.of(args[0]);
+        if (!Files.exists(path)) {
+            throw new IllegalArgumentException("Root folder is incorrect. Usage  ROOT_FOLDER.");
+        }
+        if (!args[1].startsWith(".") && !(args[1].length() > 1)) {
+            throw new IllegalArgumentException("File parameter is incorrect. Usage  .* parameter.");
+        }
+        if (!args[2].matches("^[^\\s]+\\.[^\\s.]+$")) {
+            throw new IllegalArgumentException("File name is incorrect. Usage  *.* parameter.");
+        }
+        return true;
+    }
+    public void packFiles(List<File> sources, File target) {
+        Search.search(sources, p, target)
+
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при вводе/выводе данных из файла!");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+       if (validateArgs(args)) {
+           Zip zip = new Zip();
+           zip.packSingleFile(
+                   new File("./pom.xml"),
+                   new File("./pom.zip")
+           );
+           Zip zipproj = new Zip();
+           zipproj.packFiles();
+       }
+    }
+}
