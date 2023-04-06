26d769740557cd4575206762c956728da6cf9be4
maxim <maxim5_91@mail.ru>
2023-04-07 01:18:45 +0300

    14. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -4,18 +4,14 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
     private static void validateArgs(ArgsName argsName) {
-        String dir = argsName.get("d");
+        String dir = argsName.get("d").substring(2);
         String predicate = argsName.get("e");
         String destination = argsName.get("o");
         Path path = Path.of(dir);
@@ -31,8 +27,8 @@ public class Zip {
     }
     public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (Path source :sources) {
-                zip.putNextEntry(new ZipEntry(source.toString()));
+            for (Path source : sources) {
+                zip.putNextEntry(new ZipEntry(source.toFile().toString()));
                 try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toFile()))) {
                     zip.write(out.readAllBytes());
                 }
@@ -43,40 +39,20 @@ public class Zip {
         }
     }
 
-    /**public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (IOException e) {
-            System.out.println("Ошибка при вводе/выводе данных из файла!");
-            e.printStackTrace();
-        }
-    }*/
-
     public static void main(String[] args) throws IOException {
+
         System.out.println(args.length);
-        if (!(args.length == 3)) {
+        if (args.length != 3) {
             throw new IllegalArgumentException("Some args is null.");
         }
         ArgsName argsName = ArgsName.of(args);
         validateArgs(argsName);
-        /**Zip zip = new Zip();
-        zip.packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-           );*/
-           Path path = Paths.get(argsName.get("d"));
-           Path condition = Paths.get(argsName.get("e"));
-           List<Path> list = Search.search(path, p -> !p.toString().endsWith(String.valueOf(condition)));
-           /**List<File> fileList = new ArrayList<>();
-           for (Path paths : list) {
-               fileList.add(paths.toFile());
-           }*/
-           Path path1 = Path.of(argsName.get("o"));
-           Zip zipproj = new Zip();
-           zipproj.packFiles(list, new File(path1.toString()));
+        Path path = Paths.get(argsName.get("d").substring(2));
+        String condition = Paths.get(argsName.get("e")).getFileName().toString();
+        List<Path> list = Search.search(path, p -> !(p.toString().endsWith(condition)));
+        Path path1 = Path.of(argsName.get("o"));
+        Zip zip = new Zip();
+        zip.packFiles(list, new File(path1.toString()));
 
     }
 }
