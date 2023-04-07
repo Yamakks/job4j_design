3fe43e6a3c3f201044b44d47742e016651e72f4a
yamax <ZXSWO4cu4>
2023-04-07 14:41:44 +0300

    Именованные аргументы [#295518]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -11,18 +11,18 @@ import java.util.zip.ZipOutputStream;
 public class Zip {
 
     private static void validateArgs(ArgsName argsName) {
-        String dir = argsName.get("d").substring(2);
+        String dir = argsName.get("d");
         String predicate = argsName.get("e");
         String destination = argsName.get("o");
         Path path = Path.of(dir);
         if (!Files.exists(path)) {
             throw new IllegalArgumentException("Root folder is incorrect. Usage  ROOT_FOLDER.");
         }
-        if (predicate.startsWith(".") && !(predicate.length() > 1)) {
+        if (!(predicate.startsWith(".")) && !(predicate.length() > 1)) {
             throw new IllegalArgumentException("File parameter is incorrect. Usage  .* parameter.");
         }
-        if ("^[^\\s]+\\.[^\\s.]+$".matches(destination)) {
-            throw new IllegalArgumentException("File name is incorrect. Usage  *.* parameter.");
+        if (!(destination.endsWith(".zip")) && !(destination.length() > 4)) {
+            throw new IllegalArgumentException("File name is incorrect. Usage  *.zip parameter.");
         }
     }
     public void packFiles(List<Path> sources, File target) {
@@ -40,19 +40,15 @@ public class Zip {
     }
 
     public static void main(String[] args) throws IOException {
-
-        System.out.println(args.length);
         if (args.length != 3) {
             throw new IllegalArgumentException("Some args is null.");
         }
         ArgsName argsName = ArgsName.of(args);
         validateArgs(argsName);
-        Path path = Paths.get(argsName.get("d").substring(2));
-        String condition = Paths.get(argsName.get("e")).getFileName().toString();
-        List<Path> list = Search.search(path, p -> !(p.toString().endsWith(condition)));
-        Path path1 = Path.of(argsName.get("o"));
+        Path path = Paths.get(argsName.get("d"));
+        List<Path> list = Search.search(path, p -> !(p.toString().endsWith(argsName.get("e"))));
         Zip zip = new Zip();
-        zip.packFiles(list, new File(path1.toString()));
+        zip.packFiles(list, new File(argsName.get("o")));
 
     }
 }
