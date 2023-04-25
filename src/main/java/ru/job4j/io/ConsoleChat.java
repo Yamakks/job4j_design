60a2af6be5f1666f9959d29501d27338a5f7d449
yamax <ZXSWO4cu4>
2023-04-25 14:33:24 +0300

    16. Кодировка [#862]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -1,5 +1,9 @@
 package ru.job4j.io;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConsoleChat {
@@ -19,11 +23,26 @@ public class ConsoleChat {
     }
 
     private List<String> readPhrases() {
-        return null;
+        List<String> answers = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(path, StandardCharsets.UTF_8))) {
+            br.lines().map(s -> s + System.lineSeparator()).forEach(answers::add);
+        } catch (IOException e) {
+            System.out.println("Ошибка при вводе/выводе данных из файла!");
+            e.printStackTrace();
+        }
+        return answers;
     }
 
     private void saveLog(List<String> log) {
-
+        try (PrintWriter out = new PrintWriter(
+                new FileWriter(path, StandardCharsets.UTF_8, true))) {
+            for (String string : log) {
+                out.println(string);
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при вводе/выводе данных из файла!");
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
