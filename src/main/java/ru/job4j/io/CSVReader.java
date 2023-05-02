bc152f9e1a691764b2857e56a5f14ad3595e7196
yamax <ZXSWO4cu4>
2023-05-02 14:12:29 +0300

    17. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,7 +1,14 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
+        var file = File.createTempFile("temp", null);
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
+            out.write(argsName.get("path").getBytes());
     }
     public static void main(String[] args) {
         /* здесь добавьте валидацию принятых параметров*/
