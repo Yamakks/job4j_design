4f503366b507b38a16c1331b72be350de11d2393
yamax <ZXSWO4cu4>
2023-05-04 14:34:34 +0300

    17. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -3,37 +3,78 @@ package ru.job4j.io;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
-        var file = File.createTempFile("temp", null);
-        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
-            out.write(argsName.get("path").getBytes());
+        String delimiter = argsName.get("delimiter");
+        Scanner scanner = new Scanner(new File(argsName.get("path")));
+        String line = scanner.nextLine();
+        List<Integer> integers = checkColumnForDelete(line, argsName.get("filter"), delimiter);
+        String rsl = null;
+        boolean firstLine = true;
+        while (scanner.hasNextLine()) {
+            if (firstLine) {
+                firstLine = false;
+                rsl = generatingString(line, integers, delimiter)
+                        + System.lineSeparator();
+                continue;
+            }
+            rsl = rsl.concat(generatingString(scanner.nextLine(), integers, delimiter)
+                    + System.lineSeparator());
+        }
+        printResult(rsl, argsName);
+    }
+
+    private static String generatingString(String line, List<Integer> cellForDelete, String delimiter) {
+        List<String> tmp = List.of(line.split(delimiter));
+        return tmp.stream()
+                .filter(s -> cellForDelete.contains(tmp.indexOf(s)))
+                .reduce((s1, s2) -> s1 + delimiter + s2).get();
+    }
+
+    private static List<Integer> checkColumnForDelete(String firstLine, String columnForDelete, String delimiter) {
+        List<String> arr = List.of(firstLine.split(delimiter));
+        return Stream.of(columnForDelete.split(","))
+                .filter(arr::contains)
+                .map(arr::indexOf)
+                .toList();
+    }
+
+    private static void validateArgs(ArgsName argsName) {
+        File f = new File(argsName.get("path"));
+        if (!f.exists() || !f.isFile()) {
+            throw new IllegalArgumentException("Указанный файл не существует или не является файлом");
+        }
+        if (!argsName.get("path").endsWith(".csv")) {
+            throw new IllegalArgumentException("Указан не csv-файл");
+        }
+        if ("\";\"".equals(argsName.get("delimiter"))) {
+            throw new IllegalArgumentException("Указан разделитель не для CSV-файла");
         }
     }
-    private static boolean validateArgs(String[] args) {
+    private static void printResult(String rsl, ArgsName argsName) {
+        if ("stdout".equals(argsName.get("out"))) {
+            System.out.println(rsl);
+        } else {
+            try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(argsName.get("out")))) {
+                out.write(rsl.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
         if (args.length != 4) {
-            throw new IllegalArgumentException("Some args is null.");
+            throw new IllegalArgumentException("Неправильное количество аргументов");
         }
-        if (!".csv".contains(args[0])) {
-            throw new IllegalArgumentException("File is incorrect. Usage  ROOT_FOLDER.");
-        }
-        if ((args[1].split("=")[1].length() == 0) && !(args[1].length() > 3)) {
-            throw new IllegalArgumentException("Delimiter parameter is incorrect.");
-        }
-        if ((args[2].split("=")[1].length() == 0) && !(args[2].length() > 3)) {
-            throw new IllegalArgumentException("Output parameter is incorrect.");
-        }
-        if ((args[3].split("=")[1].length() == 0) && !(args[2].length() > 3)) {
-            throw new IllegalArgumentException("Filter parameter is incorrect.");
-        }
-        return true;
-    }
-    public static void main(String[] args) {
-        /* здесь добавьте валидацию принятых параметров*/
         ArgsName argsName = ArgsName.of(args);
+        validateArgs(argsName);
         handle(argsName);
     }
 }
