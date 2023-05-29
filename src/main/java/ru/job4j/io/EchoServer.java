0e7a1f9f263d04dbe11ed97497fa07ed9324bf10
maxim <maxim5_91@mail.ru>
2023-05-30 00:40:46 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -14,27 +14,29 @@ public class EchoServer {
 
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9000)) {
-            while (!server.isClosed()) {
-                Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String s = in.readLine();
-                    s = s.split(" ")[1].split("=")[1];
-                    switch (s) {
-                        case ("Hello") -> out.write("Hello".getBytes());
-                        case ("Exit") -> server.close();
-                        default -> out.write(s.getBytes());
-                    }
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        System.out.println(str);
-                    }
-                    out.flush();
-                }
-            }
+    public static void main(String[] args) {
+            try (ServerSocket server = new ServerSocket(9000)) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream();
+                         BufferedReader in = new BufferedReader(
+                                 new InputStreamReader(socket.getInputStream()))) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        String s = in.readLine();
+                        s = s.split(" ")[1].split("=")[1];
+                        switch (s) {
+                            case ("Hello") -> out.write("Hello".getBytes());
+                            case ("Exit") -> server.close();
+                            default -> out.write(s.getBytes());
+                        }
+                        for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+                            System.out.println(str);
+                        }
+                        out.flush();
         }
+                }
+    } catch (IOException e) {
+                e.printStackTrace();
+            }
     }
 }
