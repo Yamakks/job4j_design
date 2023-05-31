3daf55ca881a94e654e200635214b9f5faa4328a
yamax <ZXSWO4cu4>
2023-05-31 14:51:26 +0300

    4. Slf4j - вывод exception. [#268853 # [#268853 #419879]]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -12,7 +12,7 @@ import java.net.Socket;
 
 public class EchoServer {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
 
     public static void main(String[] args) {
             try (ServerSocket server = new ServerSocket(9000)) {
