e7521218f54e2bf40b1945724735f11633603849
maxim <maxim5_91@mail.ru>
2024-01-03 19:40:05 +0300

    3. Поиск файлов по критерию [#783]

--- src/main/java/ru/job4j/io/ioexam/Main.java
+++ src/main/java/ru/job4j/io/ioexam/Main.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Some args is null.");
+        }
         ArgsName argsOf = ArgsName.of(args);
         Arg.find(argsOf);
     }
