697e21e604447660a8609b5362fa76249004cee6
maxim <maxim5_91@mail.ru>
2024-01-07 23:49:45 +0300

    0. Select [#6861]

--- db/fauna.sql
+++ db/fauna.sql
@@ -17,4 +17,4 @@ values ('bigboom', 20000),
 select * from fauna where name like '%fish%';
 select * from fauna where avg_age >= 10000 and avg_age <= 21000;
 select * from fauna where discovery_date is null;
-select * from fauna where discovery_date < '1950-01-01';
+select name from fauna where discovery_date < '1950-01-01';
