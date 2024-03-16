a482e5b1a5ad8b66120340d2f228d58df0948db0
maxim <maxim5_91@mail.ru>
2024-03-17 00:52:36 +0300

    0.1. Statement [#379306]_метод инициализации

---
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,76 @@
+package ru.job4j.jdbc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.StringJoiner;
+
+public class TableEditor implements AutoCloseable {
+    private Connection connection;
+
+    private Properties properties;
+
+    public TableEditor(Properties properties) throws SQLException {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() throws SQLException {
+        Properties config = new Properties();
+        try (InputStream in = TableEditor.class.getClassLoader().getResourceAsStream("app.properties")) {
+            config.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String url = config.getProperty("url");
+        String login = config.getProperty("login");
+        String password = config.getProperty("password");
+        connection = DriverManager.getConnection(url, login, password);
+
+    }
+
+    public void createTable(String tableName) {
+    }
+
+    public void dropTable(String tableName) {
+    }
+
+    public void addColumn(String tableName, String columnName, String type) {
+    }
+
+    public void dropColumn(String tableName, String columnName) {
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+    }
+
+
+    public String getTableScheme(String tableName) throws Exception {
+        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
+        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
+        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
+        buffer.add(header);
+        try (var statement = connection.createStatement()) {
+            var selection = statement.executeQuery(String.format(
+                    "SELECT * FROM %s LIMIT 1", tableName
+            ));
+            var metaData = selection.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                buffer.add(String.format("%-15s|%-15s%n",
+                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
+                );
+            }
+        }
+        return buffer.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
